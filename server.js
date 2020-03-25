bf7ab282a0eac70e65b35e28a5556f6f24d59036
jenniferslagt <45489420+jenniferslagt@users.noreply.github.com>
2020-03-25 13:05:21 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path')
+// const revManifest = require('./static/rev-manifest')
+
+const path = require('path');
 const darkSkyData = require('./modules/api.js');
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}!`))
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -13,33 +14,33 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', async (req, res) => {
+    console.log('Getting the data..')
     const data = await darkSkyData.getData()
+    const locations = require('./modules/locations.json')
 
-    res.render('index', {
+    res.render('index.ejs', {
         timezone: data.timezone,
-        summary: data.currently.summary
+        locations: locations
     });
-
 });
 
+app.get('/details/:lat/:long', async (req, res) => {
+    const lat = req.params.lat;
+    const long = req.params.long;
 
-app.get('/details', async (req, res) => {
-    const data = await darkSkyData.getData()
-    console.log('daaataa', data)
+    const data = await darkSkyData.getData(lat, long);
+    const locations = require('./modules/locations.json');
+
+    console.log('nu', data.currently)
+
     res.render('details', {
         timezone: data.timezone,
         summary: data.currently.summary,
-        // temperature: data.currenly.temperature
+        temperature: data.currently.temperature,
+        windSpeed: data.currently.windSpeed,
+        uvIndex: data.currently.uvIndex,
+        cloudCover: data.currently.cloudCover
     });
-
 });
 
-
-
-
-
-// // .send is a method which gets send to the webpage. U can put html within it.
-// app.get('/', function (req, res) {
-//     res.send('HALLOOOOTJES')
-//     console.log("hain")
-// })
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
